bf13b4a83e226afa85149e44a6745b9e05392624
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-04 16:07:15 -0500

    adding js, selecting all cells

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+//grab all cells
+const cells = document.querySelectorAll(".cell");
+//click on cell
+console.log(cells);
+//change color of cell on click
+//only click cell once
+//reset button
