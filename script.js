b6b8bd0774647e421341d665d78015ebe8b232af
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-04 16:18:20 -0500

    added color changing click function

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ console.log("Hello World");
 //grab all cells
 const cells = document.querySelectorAll(".cell");
 //click on cell
-console.log(cells);
+for (i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", function(e) {
+    e.target.style.backgroundColor = "#ffc79d";
+  });
+}
 //change color of cell on click
 //only click cell once
 //reset button
